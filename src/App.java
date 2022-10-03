985d9827e5ba3b3595776c59355d777daaa304c6
Kelly Barbosa <barbosakelly907@gmail.com>
2022-10-03 19:26:47 -0300

    Exercicio 2

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    int n;
+
+    System.out.println("Digite um número");
+    n = entrada.nextInt();
+
+    System.out.println("O número informado foi "+ n);
+       
+    }
+}
